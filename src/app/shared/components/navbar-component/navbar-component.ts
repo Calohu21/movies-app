460134e4c566c81169311c0fe00caf728a6ba9c6
Carmelo <carmelopola@hotmail.com>
2025-11-09 13:18:59 +0100

    feat: implement search movies FormControl in navbar-component.ts

--- src/app/shared/components/navbar-component/navbar-component.ts
+++ src/app/shared/components/navbar-component/navbar-component.ts
@@ -1,13 +1,39 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnDestroy, OnInit } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { MovieService } from '../../../movies/service/movie.service';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { debounceTime, distinctUntilChanged, Subject, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-navbar-component',
-  imports: [RouterLink, RouterLinkActive],
+  imports: [RouterLink, RouterLinkActive, ReactiveFormsModule],
   templateUrl: './navbar-component.html',
   styles: ``,
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit, OnDestroy {
   movieService = inject(MovieService);
+  searchControl = new FormControl('');
+  private destroy$ = new Subject<void>();
+
+  ngOnInit(): void {
+    this.searchControl.valueChanges
+      .pipe(debounceTime(400), distinctUntilChanged(), takeUntil(this.destroy$))
+      .subscribe((query) => {
+        if (query && query.trim().length >= 2) {
+          this.movieService.searchMovies(query.trim()).subscribe();
+        } else if (!query || query.trim().length === 0) {
+          this.movieService.clearSearch();
+        }
+      });
+  }
+
+  clearSearch(): void {
+    this.searchControl.setValue('');
+    this.movieService.clearSearch();
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
 }
