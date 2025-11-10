20423d4b57b066447da40ff38fcb1808a4e8fb7a
Carmelo <carmelopola@hotmail.com>
2025-11-10 13:30:57 +0100

    feat: add movie carousel

--- src/app/movies/models/carousel.interface.ts
+++ src/app/movies/models/carousel.interface.ts
@@ -1,12 +1,13 @@
 export type CarouselEndpoint =
   | 'popular'
   | 'top_rated'
+  | 'trending_day'
   | 'trending_week'
   | 'genre'
   | 'now_playing'
   | 'upcoming';
 
-export interface CarouselInterface {
+export interface CarouselConfig {
   id: string;
   title: string;
   endpoint: CarouselEndpoint;
